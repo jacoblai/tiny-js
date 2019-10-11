17ffcbfa680279cbd73b7cde9c061eabf5d1fba6
jac <5241871@qq.com>
2019-10-11 13:56:20 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.json({ ok: 0, n: 0, err: err.message });
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const graphqlHTTP = require('express-graphql');
+var {buildSchema} = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+    },
+};
+
+router.post('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+module.exports = router;
