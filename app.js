5ced4ef9112f360269bc544bf389e526d8683415
jac <5241871@qq.com>
2019-10-12 13:39:34 +0800

    auth function

--- app.js
+++ app.js
@@ -1,31 +1,125 @@
-var express = require('express');
-var logger = require('morgan');
+let express = require('express');
+const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+let logger = require('morgan');
+const gql = require('graphql-tag');
+const {buildASTSchema} = require('graphql');
 
-var indexRouter = require('./routes/index');
+let MongoClient = require('mongodb').MongoClient;
+let mongo;
+MongoClient.connect("mongodb://localhost:27017", {
+    server: {
+        poolSize: 20000
+    }
+}, function (err, db) {
+    if (err === null) {
+        mongo = db;
+        console.log("Connected correctly to server");
+    } else {
+        console.log("Connect Error " + err);
+    }
+});
 
-var app = express();
+let app = express();
+app.use(cors());
 
 function defaultContentTypeMiddleware(req, res, next) {
     req.headers['content-type'] = req.headers['content-type'] || 'application/json';
     next();
 }
+
 app.use(defaultContentTypeMiddleware);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.all('*', function (req, res, next) {
-    if (!req.get('Origin')) return next();
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.set('Access-Control-Allow-Headers', 'U-ApiKey, Content-Type');
-    //res.set('Access-Control-Allow-Max-Age', 3600);
-    if ('OPTIONS' === req.method) return res.status(200).end();
+function auth(req, res, next) {
+    let uid = req.header("Authentication");
+    if (uid === "jacoblai") {
+        req.headers["role"] = "admin";
+    }
+    console.log('uid:', uid);
     next();
-});
+}
 
-app.use('/', indexRouter);
+app.use(auth);
+
+// Construct a schema, using GraphQL schema language
+var schema = buildASTSchema(gql`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
+    deleteMessage(id: ID!): String
+  }
+`);
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+    constructor(id, {content, author}) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+}
+
+// Maps username to content
+var fakeDatabase = {};
+
+var root = {
+    getMessage: function ({id}, req) {
+        let role = req.header("role");
+        console.log(role);
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        return new Message(id, fakeDatabase[id]);
+    },
+    createMessage: function ({input}) {
+        // Create a random id for our "database".
+        var id = require('crypto').randomBytes(10).toString('hex');
+
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
+    updateMessage: function ({id, input}) {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        // This replaces all old data, but some apps might want partial update.
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
+    deleteMessage: function ({id}) {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        delete fakeDatabase[id];
+        return "ok";
+    }
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+    pretty: true,
+}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -41,7 +135,7 @@ app.use(function (err, req, res, next) {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     res.status(err.status || 500);
-    res.json({ ok: 0, n: 0, err: err.message });
+    res.json({ok: 0, n: 0, err: err.message});
 });
 
 module.exports = app;
